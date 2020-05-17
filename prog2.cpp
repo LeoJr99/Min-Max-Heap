9f6de6eef073b26d0e344f0dd35763b23b84e459
Leo <lvaldivia.jr@gmail.com>
2020-05-16 21:35:16 -0700

    Corrected file name PA2_dataset

--- prog2.cpp
+++ prog2.cpp
@@ -27,7 +27,7 @@ int main(int argc, char *argv[]) {
 
 
     ifstream textFile; 
-    textFile.open("/autograder/submission/PA1_dataset.txt"); 
+    textFile.open("/autograder/submission/PA2_dataset.txt"); 
     //textFile.open("PA2_dataset.txt");
     char charNum[100];
     int count; 
